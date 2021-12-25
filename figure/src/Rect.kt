0d140c923dcfce5eec3fbe32bce3b7ac54953348
Delll777 <58356549+Delll777@users.noreply.github.com>
2021-12-25 23:04:46 +0800

    add transform

---
+++ figure/src/Rect.kt
@@ -0,0 +1,36 @@
+// сочетание определения класса и конструктора одновременно объявляет переменные и задаёт их значения
+class Rect(var x: Int, var y: Int, var width: Int, var height: Int) : Movable, Transforming, Figure(0) {
+
+    constructor(rect: Rect) : this(rect.x, rect.y, rect.width, rect.height)
+
+    override fun move(dx: Int, dy: Int) {
+        x += dx
+        y += dy
+    }
+
+    override fun area(): Float {
+        return (width*height).toFloat() // требуется явное приведение к вещественному числу
+    }
+
+    override fun resize(zoom: Int) {
+        width *= zoom
+        height *= zoom
+    }
+
+    override fun rotate(direction: RotateDirection, centerX: Int, centerY: Int) {
+        width = height.also { height = width }
+        if (centerX == x && centerY == y) { return }
+
+        if(direction == RotateDirection.Clockwise) {
+            x = y - centerY + centerX.also { y = -1 * (x - centerX) + centerY }
+        } else {
+            x = -1 * (y - centerY) + centerX.also { y = x - centerX + centerY }
+        }
+
+
+    }
+
+    override fun toString(): String {
+        return "Rect {x: $x, y: $y, width: $width, height: $height}"
+    }
+}
